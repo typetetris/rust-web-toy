42d1fe069148444f8ad59630eac75603322e37fc
Eric Wolf <eric.wolf@lindenbaum.eu>
2021-07-02 07:38:51 +0200

    Initial

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use tokio::fs::OpenOptions;
+use tokio::io::AsyncWriteExt;
+use futures_util::stream::StreamExt;
+
+async fn manual_hello(mut body: web::Payload, web::Path(filename): web::Path<String>) -> impl Responder {
+    let mut file = OpenOptions::new().write(true).append(true).create(true).open(filename).await.unwrap();
+    while let Some(item) = body.next().await {
+        let bytes = item.unwrap();
+        file.write_all(&bytes[..]).await.unwrap();
+    }
+    file.flush().await.unwrap();
+    drop(file);
+    HttpResponse::Ok()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(
+            web::resource("/{file}")
+                .route(web::post().to(manual_hello))
+            )
+    })
+    .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
