b8edf66b3f3d80a4a12d29de377cdc2fc7040801
Eric Wolf <eric.wolf@lindenbaum.eu>
2021-07-13 08:30:28 +0200

    Add locked counter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::ops::Add;
+use std::sync::RwLock;
+use std::sync::Arc;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use futures_util::stream::StreamExt;
 use tokio::fs::OpenOptions;
@@ -36,12 +39,36 @@ async fn produce_error_response() -> impl Responder {
         .body("canned error response for testing purposes")
 }
 
+async fn read_the_counter(data: web::Data<Arc<RwLock<u32>>>) -> impl Responder {
+    let mut result = String::new();
+    {
+        let handle = data.read().unwrap();
+        result.push_str(&format!("{}", *handle));
+    }
+    HttpResponse::Ok().content_type("text/plain").body(result)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let guarded_counter: Arc<RwLock<u32>> = Arc::new(std::sync::RwLock::new(0));
+    {
+        let guarded_counter_for_background_thread = guarded_counter.clone();
+        tokio::spawn(async move {
+            loop {
+                {
+                    let mut handle = guarded_counter_for_background_thread.write().unwrap();
+                    *handle += 1;
+                }
+                tokio::time::delay_until(tokio::time::Instant::now().add(tokio::time::Duration::from_millis(250))).await;
+            }
+        });
+    }
+    HttpServer::new(move || {
         App::new()
+            .data(guarded_counter.clone())
             .service(web::resource("/upload/{file}").route(web::post().to(file_upload)))
             .route("/error", web::post().to(produce_error_response))
+            .route("/token/conference/status", web::get().to(read_the_counter))
     })
     .bind("127.0.0.1:8080")?
     .run()
