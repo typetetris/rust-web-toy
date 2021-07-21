516ea7058ed424caaaa58978d4dd31e17cbecdf3
Eric Wolf <eric.wolf@lindenbaum.eu>
2021-07-21 07:44:48 +0200

    Add a command line option

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,16 @@ use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use futures_util::stream::StreamExt;
 use tokio::fs::OpenOptions;
 use tokio::io::AsyncWriteExt;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt()]
+struct Opt {
+    /// Socket addresses to listen on for requests
+    /// e.g. 127.0.0.1:8080
+    #[structopt(short, long, required(true))]
+    socket_addrs: Vec<std::net::SocketAddr>,
+}
 
 async fn file_upload(
     mut body: web::Payload,
@@ -48,8 +58,13 @@ async fn read_the_counter(data: web::Data<Arc<RwLock<u32>>>) -> impl Responder {
     HttpResponse::Ok().content_type("text/plain").body(result)
 }
 
+async fn noop() -> impl Responder {
+    HttpResponse::Ok()
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let opt = Opt::from_args();
     let guarded_counter: Arc<RwLock<u32>> = Arc::new(std::sync::RwLock::new(0));
     {
         let guarded_counter_for_background_thread = guarded_counter.clone();
@@ -63,14 +78,16 @@ async fn main() -> std::io::Result<()> {
             }
         });
     }
-    HttpServer::new(move || {
+    let mut server = HttpServer::new(move || {
         App::new()
             .data(guarded_counter.clone())
-            .service(web::resource("/upload/{file}").route(web::post().to(file_upload)))
-            .route("/error", web::post().to(produce_error_response))
-            .route("/token/conference/status", web::get().to(read_the_counter))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+//            .service(web::resource("/upload/{file}").route(web::post().to(file_upload)))
+//            .route("/error", web::post().to(produce_error_response))
+            .route("/counter", web::get().to(read_the_counter))
+            .route("/noop", web::get().to(noop))
+    });
+    for addr in opt.socket_addrs.iter() {
+        server = server.bind(addr).unwrap();
+    }
+    server.run().await
 }
